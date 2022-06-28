46fdc31fa763611039e9f1a82fd9149168030289
Jafarammer <jafalkaff96@gmail.com>
2022-06-28 20:40:06 +0700

    make file user crud

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 8000;
+const cors = require("cors");
+const helmet = require("helmet");
+const dotenv = require("dotenv");
+dotenv.config();
+
+app.use(cors());
+app.use(helmet());
+
+//import body parser
+const bodyParser = require("body-parser");
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+// const userRoutes = require("./routes/user/userRoutes");
+// app.use("/", userRoutes);
+
+// import routes users
+const userRoutes = require("./routes/user/user");
+app.use("/", userRoutes);
+
+// const userFindRoutes = require("./routes/user/searchUser");
+// app.use("/", userFindRoutes);
+
+// Import routes recipe
+// const recipeRoutes = require("./routes/recipe");
+// app.use("/", recipeRoutes);
+
+// PORT take in bottom
+app.listen(port, () => {
+  console.log(`app running at http://localhost:${port}`);
+});
