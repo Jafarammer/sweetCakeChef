593a2db831566e9d68ba9bbb9f50b91a340bcb13
Jafarammer <jafalkaff96@gmail.com>
2022-09-10 05:29:38 +0700

    setup cors

--- index.js
+++ index.js
@@ -20,37 +20,41 @@ const uploadRoutes = require("./routes/upload/index");
 // helmet
 app.use(helmet());
 // cors
-// var allowlist = "http://localhost:3000/";
-var allowlist = "https://sweet-cake-react.web.app/";
-// var allowlist = ["https://sweet-cake-react.web.app/", "http://localhost:3000/"];
-var corsOptionsDelegate = function (req, callback) {
-  var corsOptions;
-  if (allowlist.indexOf(req.header("Origin")) !== -1) {
-    corsOptions = { origin: true };
-  } else {
-    corsOptions = { origin: false };
-  }
-  callback(null, corsOptions);
-};
+// // var allowlist = "http://localhost:3000/";
+// var allowlist = "https://sweet-cake-react.web.app/";
+// // var allowlist = ["https://sweet-cake-react.web.app/", "http://localhost:3000/"];
+// var corsOptionsDelegate = function (req, callback) {
+//   var corsOptions;
+//   if (allowlist.indexOf(req.header("Origin")) !== -1) {
+//     corsOptions = { origin: true };
+//   } else {
+//     corsOptions = { origin: false };
+//   }
+//   callback(null, corsOptions);
+// };
 //import body parser
 const bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 // app.use("/", cors(corsOptionsDelegate), userRoutes);
+const corsOptions = {
+  origins: "https://sweet-cake-react.web.app/",
+};
+app.use(cors(corsOptions));
 
 // user
-app.use("/users", cors(corsOptionsDelegate), userRoutes);
-app.use("/users", cors(corsOptionsDelegate), searchUserRoutes);
+app.use("/users", userRoutes);
+app.use("/users", searchUserRoutes);
 // auth
-app.use("/", cors(corsOptionsDelegate), authRoutes);
-// app.use("/", cors(corsOptionsDelegate), userLogin); example
+app.use("/", authRoutes);
+// app.use("/", userLogin); example
 // routes recipe
-app.use("/recipe", cors(corsOptionsDelegate), searchRecipeRoutes);
-app.use("/recipe", cors(corsOptionsDelegate), recipeRoutes);
+app.use("/recipe", searchRecipeRoutes);
+app.use("/recipe", recipeRoutes);
 // image upload
 app.use("/images", express.static("images"));
-app.use("/upload", cors(corsOptionsDelegate), uploadRoutes);
+app.use("/upload", uploadRoutes);
 
 app.use("*", (req, res) => {
   res.send("I'm Jafarammer Success");
